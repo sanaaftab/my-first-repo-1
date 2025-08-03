b9eeb0237e11ce3cf33dc90dd6ec8b3cf33fab94
Sana Aftab <sana.s.aftab@gmail.com>
2025-08-03 16:14:15 -0400

    added test cases

--- test/rps_test.py
+++ test/rps_test.py
@@ -8,7 +8,6 @@ def test_winner():
     assert determine_winner("rock", "rock") == "TIE GAME"
     assert determine_winner("paper", "paper") == "TIE GAME"
     assert determine_winner("scissors", "scissors") == "TIE GAME"
-
     assert determine_winner("rock", "paper") == "COMP WINS"
     assert determine_winner("rock", "scissors") == "USER WINS"
     assert determine_winner("paper", "rock") == "USER WINS"
